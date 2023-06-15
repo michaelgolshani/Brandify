7d36d6724b084d1b9e42a5fb946a389c912ef237
Michael Golshani <michaelgolshani@gmail.com>
2023-06-15 08:48:16 -0700

    added styling to main landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
+import './LandingPage.css'
+import headerImage from '../../assets/header.jpeg'
+
 
 
 const LandingPage = () => {
@@ -8,15 +11,24 @@ const LandingPage = () => {
 
 
   return (
-   <div>
-      <h1>The Global commerce platform</h1>
-      <h2>Build your business with Brandify to sell online, offline, and everywhere in between.</h2>
-      <NavLink to= "/login">
-        <button>Sign Up</button>
-        </NavLink>
+    <div className='landing-page-container'>
+      <div className='landing-page-top-container'>
+        <div className='landing-page-left-side'>
+          <div className='landing-page-header'>The Global commerce platform</div>
+          <h2>Build your business with Brandify to sell online, offline, and everywhere in between.</h2>
+          <NavLink to="/login">
+            <button>Sign Up</button>
+          </NavLink>
+        </div>
+        <div className='landing-page-right-side'>
+          <img src={headerImage} />
+        </div>
+      </div>
 
-      <h2>Discover why millions of entrepreneurs chose Shopify to build their business — from hello world to IPO.</h2>
-   </div>
+      <div className='landing-page-middle-container'>
+        <h2 className='landing-page-middle-container-text'>Discover why millions of entrepreneurs chose Shopify to build their business — from hello world to IPO.</h2>
+      </div>
+    </div>
   )
 }
 
