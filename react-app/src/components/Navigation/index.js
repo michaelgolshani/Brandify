7c02e6bd1c85b249f9a0c885cdfca3e79bfc40cc
Michael Golshani <michaelgolshani@gmail.com>
2024-01-26 16:29:29 -0800

    accessibility improvements for Nav

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -18,48 +18,49 @@ function Navigation({ isLoaded }) {
 	}, [dispatch, brandName])
 
 	return (
-		<ul className='nav-container'>
-			<li className='nav-item nav-item-left nav-logo'>
-				{brandName && sessionUser ? (
-					<NavLink exact to={`/store-dashboard/${brandName}`} className="nav-link">
-						<img src={logo} className="logo-image" />
-						<div className='nav-logo-text'>brandify</div>
-					</NavLink>
-				) : (
-					<>
-						<NavLink exact to="/" className="nav-link">
-							<img src={logo} className="logo-image" />
+		<nav className='full-nav-container'>
+			<ul className='nav-container'>
+				<li className='nav-item nav-item-left nav-logo'>
+					{brandName && sessionUser ? (
+						<NavLink exact to={`/store-dashboard/${brandName}`} className="nav-link" aria-label="Home">
+							<img src={logo} className="logo-image" alt="Brandify Logo" />
 							<div className='nav-logo-text'>brandify</div>
 						</NavLink>
+					) : (
+						<>
+							<NavLink exact to="/" className="nav-link" aria-label="Home">
+								<img src={logo} className="logo-image" alt="Brandify Logo" />
+								<div className='nav-logo-text'>brandify</div>
+							</NavLink>
 
-					</>
-				)}
-			</li>
-
-
-
-			{isLoaded && sessionUser ? (
-				<li className='nav-item nav-item-right'>
-					<NavLink exact to="/explore/brands" className="nav-link">
-
-						<div className='nav-logo-explore'>Explore Brands</div>
-					</NavLink>
-					<ProfileButton user={sessionUser} />
+						</>
+					)}
 				</li>
-			) : (
-				<div className='nav-login-signup-container'>
-					<NavLink exact to="/explore/brands" className="nav-link">
-						<div className='nav-logo-explore'>Explore Brands</div>
-					</NavLink>
-					<NavLink to="/login" className='nav-login'>
-						<div >Log in</div>
-					</NavLink>
-					<NavLink to="/signup" className='nav-signup'>
-						<div >Sign up</div>
-					</NavLink>
-				</div>
-			)}
-		</ul>
+
+
+
+				{isLoaded && sessionUser ? (
+					<li className='nav-item nav-item-right'>
+						<NavLink exact to="/explore/brands" className="nav-link" aria-label="Explore Brands">
+							<div className='nav-logo-explore'>Explore Brands</div>
+						</NavLink>
+						<ProfileButton user={sessionUser} />
+					</li>
+				) : (
+					<div className='nav-login-signup-container'>
+						<NavLink exact to="/explore/brands" className="nav-link" aria-label="Explore Brands">
+							<div className='nav-logo-explore'>Explore Brands</div>
+						</NavLink>
+						<NavLink to="/login" className='nav-login' aria-label="Log In">
+							<div>Log in</div>
+						</NavLink>
+						<NavLink to="/signup" className='nav-signup' aria-label="Sign Up">
+							<div>Sign up</div>
+						</NavLink>
+					</div>
+				)}
+			</ul>
+		</nav>
 	);
 }
 
